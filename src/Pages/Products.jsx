2d1f4852a35741a1f043319ae50c05d85d51ad09
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-08-31 10:41:54 +0530

    Add:Mock server miragejs Setup

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 let Products = () => {
-    return <div>
-        <h2>This is Products page</h2>
-    </div>
+  return <div>
+    <h2>This is Products page</h2>
+
+  </div>
 }
 export default Products
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
+import setupMockServer from "./api/mock.server";
+
+setupMockServer();
 
 ReactDOM.render(
   <React.StrictMode>
